89bba4ace8fd2c4e9f91dba06ca7fc1c9cc680b5
damian <damian0276@gmail.com>
2021-08-06 16:28:07 -0300

    visualisacion de la base en administrar

--- site/src/controllers/controllerAdmin.js
+++ site/src/controllers/controllerAdmin.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const faker = require('faker');
 const Product = require('../dataBase/models/product');
+const product = require('../dataBase/models/product');
 
 
 
@@ -15,11 +16,23 @@ module.exports = {
         const product = new Product();
         product.category = req.body.category_name;
         product.name = req.body.product_name;
-        product.price = req.body.product_price
+        product.price = req.body.product_price;
+        product.description = req.body.product_description;
         product.cover = faker.image.image();
+        
         product.save(err=>{
             if(err) return next(err);
             res.redirect('/addProduct');
         })
-     }
+     },
+     administrar: (req, res) => {
+
+        product.find({})
+          .then(products => {
+            res.render(path.resolve(__dirname, '..', 'views', 'admin', 'administrar'), {products});
+          })
+          .catch(err => res.send(err))
+    
+      }
+    
 }
--- site/src/dataBase/models/product.js
+++ site/src/dataBase/models/product.js
@@ -6,6 +6,8 @@ const ProductSchema = new Schema ({
         name: String,
         price: Number,
         category: String,
+        description: String,
         cover: String
+        
 });
 module.exports = mongoose.model('Product',ProductSchema);
